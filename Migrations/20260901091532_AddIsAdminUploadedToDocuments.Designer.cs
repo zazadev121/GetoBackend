2459cafff248ad63d0b23b02416df3f0ce62b1b3
zazadev121 <zazadev121@gmail.com>
2026-09-01 13:15:52 +0400

    feat: add EF Core migration for AddIsAdminUploadedToDocuments

--- Migrations/20260901091532_AddIsAdminUploadedToDocuments.Designer.cs
+++ Migrations/20260901091532_AddIsAdminUploadedToDocuments.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using apiprojnew.Data;
@@ -11,9 +12,11 @@ using apiprojnew.Data;
 namespace apiprojnew.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20260901091532_AddIsAdminUploadedToDocuments")]
+    partial class AddIsAdminUploadedToDocuments
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
